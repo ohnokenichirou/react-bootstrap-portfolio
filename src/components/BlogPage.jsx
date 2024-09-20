bcfe796bf1e9b63620160648af9d73e041a0454d
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-09-20 17:24:39 +0900

    Reactを使ったポートフォリオサイトの作り方【Bootstrap5を使用】 - プログラミングチュートリアル

---
+++ src/components/BlogPage.jsx
@@ -0,0 +1,139 @@
+import React from "react";
+
+export const BlogPage = () => {
+  return (
+    <div className="container">
+      <h1>ブログ</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A obcaecati,
+        voluptas cupiditate nulla reiciendis odit dolore esse unde ut voluptatum
+        incidunt quaerat inventore numquam assumenda! Nostrum aperiam est rerum,
+        unde veritatis excepturi nesciunt architecto nemo eum, debitis aliquid
+        a, repellat incidunt! Culpa accusantium facilis aut quia consectetur.
+        Unde et beatae repellat culpa dolor sint voluptate saepe? Ab non nihil
+        consequatur perspiciatis. Blanditiis similique dolor ab quidem
+        veritatis, sit nam. Distinctio nemo delectus, dignissimos voluptate
+        veniam cupiditate sapiente velit voluptatum praesentium laborum maxime
+        fuga. Quam ex dolorum asperiores, quas sunt fugiat, provident aliquid,
+        ipsum doloribus maxime pariatur soluta animi nam eligendi.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A obcaecati,
+        voluptas cupiditate nulla reiciendis odit dolore esse unde ut voluptatum
+        incidunt quaerat inventore numquam assumenda! Nostrum aperiam est rerum,
+        unde veritatis excepturi nesciunt architecto nemo eum, debitis aliquid
+        a, repellat incidunt! Culpa accusantium facilis aut quia consectetur.
+        Unde et beatae repellat culpa dolor sint voluptate saepe? Ab non nihil
+        consequatur perspiciatis. Blanditiis similique dolor ab quidem
+        veritatis, sit nam. Distinctio nemo delectus, dignissimos voluptate
+        veniam cupiditate sapiente velit voluptatum praesentium laborum maxime
+        fuga. Quam ex dolorum asperiores, quas sunt fugiat, provident aliquid,
+        ipsum doloribus maxime pariatur soluta animi nam eligendi.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A obcaecati,
+        voluptas cupiditate nulla reiciendis odit dolore esse unde ut voluptatum
+        incidunt quaerat inventore numquam assumenda! Nostrum aperiam est rerum,
+        unde veritatis excepturi nesciunt architecto nemo eum, debitis aliquid
+        a, repellat incidunt! Culpa accusantium facilis aut quia consectetur.
+        Unde et beatae repellat culpa dolor sint voluptate saepe? Ab non nihil
+        consequatur perspiciatis. Blanditiis similique dolor ab quidem
+        veritatis, sit nam. Distinctio nemo delectus, dignissimos voluptate
+        veniam cupiditate sapiente velit voluptatum praesentium laborum maxime
+        fuga. Quam ex dolorum asperiores, quas sunt fugiat, provident aliquid,
+        ipsum doloribus maxime pariatur soluta animi nam eligendi.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A obcaecati,
+        voluptas cupiditate nulla reiciendis odit dolore esse unde ut voluptatum
+        incidunt quaerat inventore numquam assumenda! Nostrum aperiam est rerum,
+        unde veritatis excepturi nesciunt architecto nemo eum, debitis aliquid
+        a, repellat incidunt! Culpa accusantium facilis aut quia consectetur.
+        Unde et beatae repellat culpa dolor sint voluptate saepe? Ab non nihil
+        consequatur perspiciatis. Blanditiis similique dolor ab quidem
+        veritatis, sit nam. Distinctio nemo delectus, dignissimos voluptate
+        veniam cupiditate sapiente velit voluptatum praesentium laborum maxime
+        fuga. Quam ex dolorum asperiores, quas sunt fugiat, provident aliquid,
+        ipsum doloribus maxime pariatur soluta animi nam eligendi.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A obcaecati,
+        voluptas cupiditate nulla reiciendis odit dolore esse unde ut voluptatum
+        incidunt quaerat inventore numquam assumenda! Nostrum aperiam est rerum,
+        unde veritatis excepturi nesciunt architecto nemo eum, debitis aliquid
+        a, repellat incidunt! Culpa accusantium facilis aut quia consectetur.
+        Unde et beatae repellat culpa dolor sint voluptate saepe? Ab non nihil
+        consequatur perspiciatis. Blanditiis similique dolor ab quidem
+        veritatis, sit nam. Distinctio nemo delectus, dignissimos voluptate
+        veniam cupiditate sapiente velit voluptatum praesentium laborum maxime
+        fuga. Quam ex dolorum asperiores, quas sunt fugiat, provident aliquid,
+        ipsum doloribus maxime pariatur soluta animi nam eligendi.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A obcaecati,
+        voluptas cupiditate nulla reiciendis odit dolore esse unde ut voluptatum
+        incidunt quaerat inventore numquam assumenda! Nostrum aperiam est rerum,
+        unde veritatis excepturi nesciunt architecto nemo eum, debitis aliquid
+        a, repellat incidunt! Culpa accusantium facilis aut quia consectetur.
+        Unde et beatae repellat culpa dolor sint voluptate saepe? Ab non nihil
+        consequatur perspiciatis. Blanditiis similique dolor ab quidem
+        veritatis, sit nam. Distinctio nemo delectus, dignissimos voluptate
+        veniam cupiditate sapiente velit voluptatum praesentium laborum maxime
+        fuga. Quam ex dolorum asperiores, quas sunt fugiat, provident aliquid,
+        ipsum doloribus maxime pariatur soluta animi nam eligendi.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A obcaecati,
+        voluptas cupiditate nulla reiciendis odit dolore esse unde ut voluptatum
+        incidunt quaerat inventore numquam assumenda! Nostrum aperiam est rerum,
+        unde veritatis excepturi nesciunt architecto nemo eum, debitis aliquid
+        a, repellat incidunt! Culpa accusantium facilis aut quia consectetur.
+        Unde et beatae repellat culpa dolor sint voluptate saepe? Ab non nihil
+        consequatur perspiciatis. Blanditiis similique dolor ab quidem
+        veritatis, sit nam. Distinctio nemo delectus, dignissimos voluptate
+        veniam cupiditate sapiente velit voluptatum praesentium laborum maxime
+        fuga. Quam ex dolorum asperiores, quas sunt fugiat, provident aliquid,
+        ipsum doloribus maxime pariatur soluta animi nam eligendi.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A obcaecati,
+        voluptas cupiditate nulla reiciendis odit dolore esse unde ut voluptatum
+        incidunt quaerat inventore numquam assumenda! Nostrum aperiam est rerum,
+        unde veritatis excepturi nesciunt architecto nemo eum, debitis aliquid
+        a, repellat incidunt! Culpa accusantium facilis aut quia consectetur.
+        Unde et beatae repellat culpa dolor sint voluptate saepe? Ab non nihil
+        consequatur perspiciatis. Blanditiis similique dolor ab quidem
+        veritatis, sit nam. Distinctio nemo delectus, dignissimos voluptate
+        veniam cupiditate sapiente velit voluptatum praesentium laborum maxime
+        fuga. Quam ex dolorum asperiores, quas sunt fugiat, provident aliquid,
+        ipsum doloribus maxime pariatur soluta animi nam eligendi.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A obcaecati,
+        voluptas cupiditate nulla reiciendis odit dolore esse unde ut voluptatum
+        incidunt quaerat inventore numquam assumenda! Nostrum aperiam est rerum,
+        unde veritatis excepturi nesciunt architecto nemo eum, debitis aliquid
+        a, repellat incidunt! Culpa accusantium facilis aut quia consectetur.
+        Unde et beatae repellat culpa dolor sint voluptate saepe? Ab non nihil
+        consequatur perspiciatis. Blanditiis similique dolor ab quidem
+        veritatis, sit nam. Distinctio nemo delectus, dignissimos voluptate
+        veniam cupiditate sapiente velit voluptatum praesentium laborum maxime
+        fuga. Quam ex dolorum asperiores, quas sunt fugiat, provident aliquid,
+        ipsum doloribus maxime pariatur soluta animi nam eligendi.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A obcaecati,
+        voluptas cupiditate nulla reiciendis odit dolore esse unde ut voluptatum
+        incidunt quaerat inventore numquam assumenda! Nostrum aperiam est rerum,
+        unde veritatis excepturi nesciunt architecto nemo eum, debitis aliquid
+        a, repellat incidunt! Culpa accusantium facilis aut quia consectetur.
+        Unde et beatae repellat culpa dolor sint voluptate saepe? Ab non nihil
+        consequatur perspiciatis. Blanditiis similique dolor ab quidem
+        veritatis, sit nam. Distinctio nemo delectus, dignissimos voluptate
+        veniam cupiditate sapiente velit voluptatum praesentium laborum maxime
+        fuga. Quam ex dolorum asperiores, quas sunt fugiat, provident aliquid,
+        ipsum doloribus maxime pariatur soluta animi nam eligendi.
+      </p>
+    </div>
+  );
+};
